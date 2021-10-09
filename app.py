784bbf4f03d6e5b7a9333bca5574a4748030546d
Kalash Jindal <37014842+erickeagle@users.noreply.github.com>
2021-10-09 23:30:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,11 +2,15 @@
 from sklearn import preprocessing 
 import streamlit as st
 import pandas as pd
-
+import plotly.figure_factory as ff
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pickle
+
 filename = 'final_model.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
-
+df = pd.read_csv("Clustered_Customer_Data.csv")
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.markdown('<style>body{background-color: Blue;}</style>',unsafe_allow_html=True)
 st.title("Prediction")
@@ -35,9 +39,16 @@ with st.form("my_form"):
     submitted = st.form_submit_button("Submit")
 
 if submitted:
+    clust=loaded_model.predict(data)[0]
+    print('Data Belongs to Cluster',clust)
 
-    print('Data Belongs to Cluster',loaded_model.predict(data)[0])
-
-
+    cluster_df1=df[df['Cluster']==clust]
+    plt.rcParams["figure.figsize"] = (20,3)
+    for c in cluster_df1.drop(['Cluster'],axis=1):
+        fig, ax = plt.subplots()
+        grid= sns.FacetGrid(cluster_df1, col='Cluster')
+        grid= grid.map(plt.hist, c)
+        plt.show()
+        st.pyplot(figsize=(5, 5))
 
 
